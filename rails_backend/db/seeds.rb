c47dc54300aad92fc1f592ff762bc5773c7540d6
kalunge <muthomititus+github@gmail.com>
2023-01-10 09:36:04 +0300

    Add Author resources with updated details

--- rails_backend/db/seeds.rb
+++ rails_backend/db/seeds.rb
@@ -9,3 +9,8 @@
 books = Book.create([
     { title: "The Hobbit", isbn: "9780547928227", author: "J.R.R. Tolkien", price: 10.99, description: "The Hobbit is a children's fantasy novel by English author J. R. R. Tolkien. It was published on 21 September 1937 to wide critical acclaim, being nominated for the Carnegie Medal and awarded a prize from the New York Herald Tribune for best juvenile fiction. The book remains popular and is recognized as a classic in children's literature.", year: 1937 },
 ])
+
+authors = Author.create([
+    { first_name: "J.R.R.", last_name:"Tolkien", bio: "John Ronald Reuel Tolkien, CBE FRSL was an English writer, poet, philologist, and university professor who is best known as the author of the classic high fantasy works The Hobbit, The Lord of the Rings, and The Silmarillion." },
+])
+
